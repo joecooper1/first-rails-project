0cbda46558336c554ca5167c4ab439b2adb509d7
joecooper1 <joecooper2991@gmail.com>
2020-03-24 17:02:14 +0000

    started making articles functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/Index'
+
+  resources :articles
+
+  root 'welcome#Index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
